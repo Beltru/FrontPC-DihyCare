5fd05c21e216e60e4d1baa51442cf7abb4c4c694
Beltru <belvader007@gmail.com>
2025-05-09 11:44:00 -0300

    solucionado el error de hidratacion de area chart

--- dihy_care/app/components/AreaChart.jsx
+++ dihy_care/app/components/AreaChart.jsx
@@ -1,7 +1,48 @@
 "use client";
+import { Jaini } from "next/font/google";
 import {AreaChart, Area } from "recharts"
+const productSales = [
+   { 
+ name: "Jan",
+product1: 4000,
+product2: 3000,
+},
+{   
+name: "Feb",
+product1: 5000,
+product2: 2000,
+},
+{
+name: "Mar",
+product1: 2000,
+product2: 1000,
+},
+{
+    name: "Apr",
+    product1: 3700,
+    product2: 2400,
+    },
+ {
+ name: "May",
+  product1: 7800,
+  product2: 9500,
+  },
+        {
+name: "Jun",
+product1: 2900,
+product2: 8300,
+            }
+
+]
+
+
+
 const AreaChartComponent = () => {
-return <h1>Area Chart</h1>;
-}
+return(
+<AreaChart width={500} height={400} data={productSales}>
+<Area dataKey="product1"/>
+</AreaChart>
+  ) 
+};
 
 export default AreaChartComponent;
--- dihy_care/app/page.jsx
+++ dihy_care/app/page.jsx
@@ -1,10 +1,14 @@
+"use client";
+import { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
 import Image from "next/image";
-import  AreaChart from "./components/AreaChart"; 
 import chart from "recharts";
+
+const AreaChart = dynamic(() => import('./components/AreaChart'), { ssr: false });
 export default function Home() {
   return (
-    <div className="bg-radial from-[#254A7F] to-[#140850] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <AreaChart />
+    <div className="bg-radial from-[#254A7F] to-[#140850] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <AreaChart/>
     </div>
   );
 }
