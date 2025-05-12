f7f78d81e8e9a607452aeb5307b7e307516cef47
Beltru <belvader007@gmail.com>
2025-05-12 12:09:32 -0300

    sidebar

--- dihy_care/app/page.jsx
+++ dihy_care/app/page.jsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import chart from "recharts";
 export default function Home() {
   return (
-<main className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10 py-44 bg-radial from-[#254A7F] to-[#140850] ">
-  </main>
+<main className="flex min-h-screen items-center bg-radial from-[#254A7F] to-[#140850]">
+ 
+ </main>
   );
 }
