6667b0a90cf08b15c06c10a78b8d6620ab86c6a9
Beltru <belvader007@gmail.com>
2025-05-23 11:21:49 -0300

    termine la sidebar

--- dihy_care/app/page.jsx
+++ dihy_care/app/page.jsx
@@ -40,13 +40,17 @@ return(
           <div className='mt-4 flex flex-col gap-4 relative'>
             {
               menus?.map((menu,i)=>(
-                <Link href={menu?.link} key={i} className={`${menu?.margin && "mt-5"} flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-700 rounded-md`}>
+                <Link href={menu?.link} key={i} className={`${menu?.margin && "mt-5"} group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-700 rounded-md`}>
        
             <div>
               {React.createElement(menu?.icon,{size:"20"})}
             </div>
           
-             <h2 className={`whitespace-pre duration-500 ${!open && "opacity-0 -translate-x-28 overflow-hidden"}`}>{menu?.name}</h2>
+             <h2 style={{transitionDelay: `${i + 3}00ms`,}} className={`whitespace-pre duration-500 ${!open && "opacity-0 -translate-x-28 overflow-hidden"}`}>{menu?.name}
+             </h2>
+             <h2 className={`${open && "hidden"} absolute left-48 bg-white font-semibold  whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}>
+              {menu?.name}
+             </h2>
             </Link> 
               ))        
 }
@@ -54,7 +58,7 @@ return(
           </div>
         </div>
 
-       {/*esto es elresto de la pagina*/} 
+       {/*esto es el resto de la pagina*/} 
         <div className='m-3 text-xl text-gray-900 font-semibold'>
           REACT TAILWIND
         </div>
