67e7f6a0f5e835093e775b14ea766b4c787750a7
Beltru <belvader007@gmail.com>
2025-06-13 11:53:32 -0300

    .

--- dihy_care/app/landing/page.jsx
+++ dihy_care/app/landing/page.jsx
@@ -2,20 +2,20 @@ import Link from "next/link"
 
 const Landing = () => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-700 to-blue-400">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-radial from-blue-400 to-blue-700">
       <div className="flex flex-col items-center gap-6">
-        <img src="/CorazonClaro.png" alt="" className="h-[30vh]" />
+        <img src="/CorazonOscuro.png" alt="" className="h-[30vh]"/>
         
         <div className="flex gap-6">
           <Link
             href="/register"
-            className="bg-[#d3dddf] text-black px-6 py-2 rounded-full font-bold hover:cursor-pointer hover:bg-[#909697]"
+            className="bg-[#D9D9D9] text-black px-10 py-2 rounded-full font-bold hover:cursor-pointer hover:bg-[#909697]"
           >
-            Register
+            Sign Up
           </Link>
           <Link
             href="/login"
-            className="bg-[#0db9cf] text-black px-6 py-2 rounded-full font-bold hover:cursor-pointer hover:bg-[#067787]"
+            className="bg-[#377A95] text-black px-10 py-2 rounded-full font-bold hover:cursor-pointer hover:bg-[#067787]"
           >
             Login
           </Link>
