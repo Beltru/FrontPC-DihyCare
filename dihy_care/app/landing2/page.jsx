a7cd9b97b2d951b8202d080d0a7bf68d8d6f6615
Beltru <belvader007@gmail.com>
2025-07-14 20:56:12 -0300

    .

--- dihy_care/app/landing2/page.jsx
+++ dihy_care/app/landing2/page.jsx
@@ -27,12 +27,12 @@ export default function Landing2() {
   }, []);
 
   return (
-    <div className="h-[532vh] bg-gradient-to-r from-[#0e0511] via-[#292d47] to-[#0e0511] overflow-hidden">
+    <div className="h-[532vh] bg-gradient-to-r from-[#07df9b] via-[#07a49c] to-[#069bd6] overflow-hidden">
       
       <main className="flex min-h-screen flex-col relative">
   {/* Fondo de cuadrados */}
   <div className="absolute top-0 left-0 w-full h-full z-0">
-  <Particles particleColors={['#ffffff', '#ffffff']} particleCount={200} particleSpread={10} speed={0.1} particleBaseSize={100}moveParticlesOnHover={true} alphaParticles={false} disableRotation={false}/>
+  <Particles particleColors={['#ffffff', '#ffffff']} particleCount={300} particleSpread={10} speed={0.5} particleBaseSize={100}moveParticlesOnHover={true} alphaParticles={false} disableRotation={false}/>
   </div>
 
   {/* Contenido encima del fondo */}
