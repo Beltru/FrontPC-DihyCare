e5232e83eb54409eeacb3b97a5962cb03fba69f7
Beltru <belvader007@gmail.com>
2025-11-28 11:32:03 -0300

    Revert "actualizacion del interceptor"
    This reverts commit 2e165eddc70e013c778247909f273de157a43834.

--- dihy_care/app/src/interceptors/axios.interceptor.tsx
+++ dihy_care/app/src/interceptors/axios.interceptor.tsx
@@ -1,13 +1,7 @@
 //interceptior para la llamada del backend
-import {
-  AxiosInstance,
-  InternalAxiosRequestConfig,
-  AxiosError, 
-} from 'axios'; 
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
 export const AxiosInterceptor = (instance: AxiosInstance) => {
-
-  // Lógica del Interceptor de Solicitud (Request) - (SIN CAMBIOS)
   const updateHeader = (request: InternalAxiosRequestConfig) => {
     // localStorage solo está disponible en el cliente
     const token =
@@ -30,56 +24,14 @@ export const AxiosInterceptor = (instance: AxiosInstance) => {
     updateHeader,
     (error) => Promise.reject(error)
   );
+
   instance.interceptors.response.use(
     (response) => {
-      console.log('Respuesta interceptada exitosa', response);
+      console.log('Respuesta interceptada', response);
       return response;
     },
-    (error: AxiosError) => {
-      const Dihy_Care = '[API INTERCEPTOR]';
-
-      if (error.response) {
-        const status = error.response.status;
-        const message = error.response.data || 'Error desconocido';
-
-      
-        if (status === 401) {
-          console.error(
-            `${Dihy_Care} ERROR 401: Sesión expirada o token inválido. Mensaje:`, 
-            message
-          );
-        
-        }
-
-        else if (status === 403) {
-          console.error(
-            `${Dihy_Care} ERROR 403: Acceso denegado. Mensaje:`, 
-            message
-          );
-        }
-        
-        // manejo errores de server 500+
-      
-        else if (status >= 500) {
-          console.error(
-            `${Dihy_Care} ERROR ${status}: Error interno del servidor. Mensaje:`, 
-            message
-          );
-        }
-        
-        // no manejados 
-        else {
-            console.warn(
-                `${Dihy_Care} ERROR ${status}: Error HTTP no manejado. Mensaje:`, 
-                message
-            );
-        }
-
-      } else {
-          // 
-          console.error(`${Dihy_Care} ERROR DE RED:`, error.message);
-      }
-      
+    (error) => {
+      console.error('Error interceptado', error);
       return Promise.reject(error);
     }
   );
